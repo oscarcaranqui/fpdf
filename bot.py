ca4a1330bf305db625325c8479391334af0cb617
oscar <oscardavidcaranqui@hotmail.com>
2023-02-12 13:40:25 -0500

    Archivos mqtt para impresora

---
+++ bot.py
@@ -0,0 +1,14 @@
+from v1.MqttClient_testing import MobilePhone
+
+# create and start the MQTT client
+printer_client = MobilePhone(client_name="mobile_phone",
+                            subscribe_topic="command/device1/response",
+                            publish_topic="command/device1/request"
+                            )
+printer_client.run(broker="localhost",
+                   port=1883
+                   )
+
+while True:
+    message = input("Message to send: ")
+    printer_client.send_request(message)
---
+++ main.py
@@ -0,0 +1,11 @@
+from v1.MqttClient import MQTTClient
+
+# create and start the MQTT client
+printer_client = MQTTClient(client_name="printer",
+                            subscribe_topic="command/device1/request",
+                            publish_topic="command/device1/response"
+                            )
+
+printer_client.run(broker="localhost",
+                   port=1883
+                   )
